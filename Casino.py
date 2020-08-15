c73014e60e702bff3754434ca755b4b58bd6ef6a
Jollynjose <67757929+Jollynjose@users.noreply.github.com>
2020-08-15 00:18:13 -0400

    creacion de cartas parte 1
    Comence a crear las cartas y organice las cartas Pica

--- Casino.py
+++ Casino.py
@@ -1,1 +1,14 @@
+from Cartas import barajas
+class Cartas:
+    class pica:
+        carta_pica= []
+        for pica in barajas:
+            if "Pica" in pica:
+                carta_pica.append(pica)
+maso= Cartas
+print(maso.pica.carta_pica)
+
+    
+
+
 
