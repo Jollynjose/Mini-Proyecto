7ceff004e8bc81d7fa9658a642b036df21e8f9da
Jollynjose <67757929+Jollynjose@users.noreply.github.com>
2020-09-12 14:27:12 -0400

    9/12/2020
    ALPHA VERSION IS COMPLETE

--- Union.py
+++ Union.py
@@ -1,17 +1,15 @@
-
+from Table import *
 class Union:
     def __init__(self):
-        self.cards = []
-        self.value = self.total(self.cards)
-       
+        self.cards = Table().unions_on_table
+        
     
     def add_card(self,cards):
             cards = list(filter(self.erase_value_in_card,cards))
             value = self.total(cards)
-            print(value)
             cards.append(value)
+            print(value)
             return cards
-            
             
 
     def erase_value_in_card(self,card):
@@ -22,22 +20,12 @@ class Union:
 
     def total(self,cards):
         t = 0
-        for card in cards:
-            t = card[1] + t   
-        return t
-    # c = list(filter(lambda v: v[1] != self.value, cards))
-        # if len(c) == 0:
-        #     for card in cards:
-        #         self.cards.append(card)
-        #     return True
-
-        # newCardsTotal = self.total(cards)
-        # if newCardsTotal == self.value:
-        #     for card in cards:
-        #         self.cards.append(card)
-        #     return True
-
-        # return False     
+        if cards[0][1] == cards[1][1]:
+            return cards[0][1]
+        else:
+            for card in cards:
+                t = card[1] + t   
+            return t
     
     
 
--- main.py
+++ main.py
@@ -24,7 +24,8 @@ class Play:
     def run(self):
         while True:
             if self.no_more_cards():
-                self.Game.check_winner(self.playerOne,self.playerTwo)
+                cls()
+                print(self.Game.check_winner(self.playerOne,self.playerTwo))
                 break
             else:
                 if self.playerOne.deck_of_cards and self.playerTwo.deck_of_cards:
@@ -36,6 +37,17 @@ class Play:
                     self.Game.players_deck_of_card(self.playerOne.deck_of_cards)
                     self.Game.players_deck_of_card(self.playerTwo.deck_of_cards)
 
+    def check_duplicate(self):
+        for x in range(len(self.cards_union)):
+            if self.cards_union[x][1] == self.cards_union[x+1][1]:
+                return False
+            return True
+           
+    def checkunion(self,cards):
+        for card in self.cards_union:
+            if card == cards:
+                return False
+        return True
 
     def checktable(self,cards):
         for card in self.cards_on_table:
@@ -43,155 +55,151 @@ class Play:
                 return False
         return True
     
-    
-    
     def checkcard(self,cards):
         for card in self.check:
             if cards == card:
                 return False
         return True
-    # def union(self,cards):
-    #     for card in self.union_table:
-    #         if cards == card:
-    #             return False
-    #         elif cards == self.union_table[len(self.union_table)]:
-    #             return False
-    #     return True
-    
-        
-
-        
-    
-    
-    
-    
-
 
     def next_play(self,player):
         while True:
-            if self.cards_on_table: 
-                try:     
-                    print('Cassino Card Game',player.deck_of_cards_grabbed)
-                    print('Game Table',list(enumerate(self.cards_on_table)))
-                    print('Union',self.cards_union)
-                    print(player.name,player.deck_of_cards)
-                    print('Introduce 1 if you want to discard a card, ','Introduce 2 if you want to take a card on table', 'Introduce 3 if you want to build a union')
-                    user_action = int(input('what action would you take? : '))
+            try:
+                if self.cards_on_table:    
+                        print('Cassino The Card Game')
+                        print('Game Table',self.cards_on_table)
+                        print('Union',self.cards_union)
+                        print(player.name,player.deck_of_cards)
+                        print('Player cards grabbed', player.deck_of_cards_grabbed)
+                        print('Introduce 1 if you want to discard a card, ','Introduce 2 if you want to take a card on table, ', 'Introduce 3 if you want to build a union')
+                        user_action = int(input('what action would you take? : '))
 
-                    if user_action == 1: ##discard card
-                        print(list(enumerate(player.deck_of_cards)))
-                        user_action = int(input('Which card would you discard? '))
-                        self.cards_on_table.append((player.deck_of_cards.pop(user_action)))
-                        player.deck_of_cards = list(filter(self.checktable,player.deck_of_cards))
-                        break
-
-                        
-                        
-
-                    elif user_action == 2: ##take card on table
-                        cls()
-                        print(list(enumerate(self.cards_on_table)))
-                        print(player.name, list(enumerate(player.deck_of_cards)))
-                        print('Introduce 1 if you want to take a one card,' ,'introduce 2 if you want to take Two cards on table, ', 'Introduce 3 if you want to take a Union')
-                        user_action = int(input(''))
-
-                        if user_action == 1:
-                            user_actionOne= int(input('Which card on the table would you take? : '))
-                            user_actionTwo = int(input('Which card in your deck would you use? : '))
-                            if self.cards_on_table[user_actionOne][1] == player.deck_of_cards[user_actionTwo][1]:
-                                player.deck_of_cards_grabbed.append(self.cards_on_table[user_actionOne])
-                                player.deck_of_cards_grabbed.append(player.deck_of_cards[user_actionTwo])
-                                self.cards_on_table = list(filter(self.checkcard,self.cards_on_table))
-                                player.deck_of_cards = list(filter(self.checkcard,player.deck_of_cards))
-                                break
-
-                        elif user_action == 2:
-                            user_actionOne= int(input('Which card on the table would you take? : '))
-                            user_actionTwo = int(input('Which card on the table would you take? : '))
-                            user_actionThree = int(input('Which card in your deck would you use? : '))
-                            if self.cards_on_table[user_actionOne][1] + self.cards_on_table[user_actionTwo][1] <= 13 :
-                                if self.cards_on_table[user_actionOne][1] + self.cards_on_table[user_actionTwo][1] == player.deck_of_cards[user_actionThree][1]:
-                                    
-                                    player.deck_of_cards_grabbed.append(self.cards_on_table[user_actionOne])
-                                    player.deck_of_cards_grabbed.append(self.cards_on_table[user_actionTwo])
-                                    player.deck_of_cards_grabbed.append(player.deck_of_cards[user_actionThree])
-                                    self.cards_on_table = list(filter(self.checkcard,self.cards_on_table))
-                                    player.deck_of_cards = list(filter(self.checkcard, player.deck_of_cards))
-                                    break
-                                else:
-                                    print('Wrong card')
-
-                            else: 
-                                print('Wrong play')
-                        elif user_action == 3:
-                            print(self.union_table)
+                        if user_action == 1: ##discard card
                             print(list(enumerate(player.deck_of_cards)))
-                            user_actionOne = int(input('Which card in your deck would you use? : '))
-                            if player.deck_of_cards[user_actionOne] == self.union_table[-1]:
-                                pass
-                            else: 
-                                print( 'Wrong play')
-                            
-                        
+                            user_action = int(input('Which card would you discard? '))
+                            cls()
+                            self.cards_on_table.append((player.deck_of_cards[user_action]))
+                            player.deck_of_cards = list(filter(self.checktable,player.deck_of_cards))
+                            break     
 
-                        
-                    elif user_action == 3:
-                        print('Introduce 1 if you want to create a union, ', 'Introduce 2 if you want to increase a union ')
-                        user_action = int(input(''))
-                        if user_action == 1:
-                            if  self.cards_union:
-                                print('The game has not supported more unions')
-                            else:
-                                self.cards_union.append(player.deck_of_cards.pop(user_action))
-                                self.cards_union = self.union.add_card(self.cards_union)
-                                
-
-                        elif user_action == 2:
+                        elif user_action == 2: ##take card on table
+                            cls()
                             print(list(enumerate(self.cards_on_table)))
+                            print(player.name, list(enumerate(player.deck_of_cards)))
+                            print('Introduce 1 if you want to take a one card,' ,'introduce 2 if you want to take Two cards on table, ', 'Introduce 3 if you want to take a Union')
+                            user_action = int(input('Select your action : '))
+                            if user_action == 1:
+                                user_actionOne= int(input('Which card on the table would you take? : '))
+                                user_actionTwo = int(input('Which card in your deck would you use? : '))
+                                if self.cards_on_table[user_actionOne][1] == player.deck_of_cards[user_actionTwo][1]:
+                                    player.deck_of_cards_grabbed.append(self.cards_on_table[user_actionOne])
+                                    player.deck_of_cards_grabbed.append(player.deck_of_cards[user_actionTwo])
+                                    self.cards_on_table = list(filter(self.checkcard,self.cards_on_table))
+                                    player.deck_of_cards = list(filter(self.checkcard,player.deck_of_cards))
+                                    break
+
+                            elif user_action == 2:
+                                user_actionOne= int(input('Which card on the table would you take? : '))
+                                user_actionTwo = int(input('Which card on the table would you take? : '))
+                                user_actionThree = int(input('Which card in your deck would you use? : '))
+                                if self.cards_on_table[user_actionOne][1] + self.cards_on_table[user_actionTwo][1] <= 10 :
+                                    if self.cards_on_table[user_actionOne][1] + self.cards_on_table[user_actionTwo][1] == player.deck_of_cards[user_actionThree][1]:
+                                        player.deck_of_cards_grabbed.append(self.cards_on_table[user_actionOne])
+                                        player.deck_of_cards_grabbed.append(self.cards_on_table[user_actionTwo])
+                                        player.deck_of_cards_grabbed.append(player.deck_of_cards[user_actionThree])
+                                        self.cards_on_table = list(filter(self.checkcard,self.cards_on_table))
+                                        player.deck_of_cards = list(filter(self.checkcard, player.deck_of_cards))
+                                        break
+
+                                    else:
+                                        cls()
+                                        print('Wrong card')
+
+                                else: 
+                                    cls()
+                                    print('Wrong play')
+
+                            elif user_action == 3:
+                                if self.cards_union:
+                                    print(self.union_table)
+                                    print(list(enumerate(player.deck_of_cards)))
+                                    user_actionOne = int(input('Which card in your deck would you use? : '))
+                                    if player.deck_of_cards[user_actionOne] == self.cards_union[-1]:
+                                        self.cards_union.pop()
+                                        for x in self.cards_union:
+                                            player.deck_of_cards_grabbed.append(x)
+                                        self.cards_union = list(filter(lambda v : False ,self.cards_union))
+                                    else: 
+                                        cls()
+                                        print( 'Wrong play')
+                                else:
+                                    cls()
+                                    print('there are not cards in union')
+                            
+                        elif user_action == 3:
+                            print('Introduce 1 if you want to create a union, ', 'Introduce 2 if you want to increase a union ')
+                            user_action = int(input(''))
+                            if user_action == 1:
+                                if  self.cards_union:
+                                    print('The game has not supported more unions')
+                                else:
+                                    cls()
+                                    print(list(enumerate(self.cards_on_table)))
+                                    print(list(enumerate(player.deck_of_cards)))
+                                    user_actionOne = int(input('How card on the table you want to make a union? : '))
+                                    user_actionTwo = int(input('How card do you use in your deck cards? : '))
+                                    if self.cards_on_table[user_actionOne] + player.deck_of_cards[user_actionTwo] >= 13:
+                                        if self.cards_on_table[user_actionOne] == player.deck_of_cards[user_actionTwo]:
+                                            self.cards_union.append(self.cards_on_table.pop(user_actionOne))
+                                            self.cards_union.append(player.deck_of_cards.pop(user_actionTwo))
+                                            self.cards_union = self.union.add_card(self.cards_union)
+                                            break
+                                        else:
+                                            print('The value of the union must be less than 13')
+                                    else:
+                                        self.cards_union.append(self.cards_on_table.pop(user_actionOne))
+                                        self.cards_union.append(player.deck_of_cards.pop(user_actionTwo))
+                                        self.cards_union = self.union.add_card(self.cards_union)
+                                        break
+                                    
+
+                            elif user_action == 2:
+                                if self.cards_union:
+                                    if self.cards_union[-1] <= 10:
+                                        print('How much do you want incremate?')
+                                        print(self.cards_union)
+                                        print(list(enumerate(player.deck_of_cards)))
+                                        user_actionOne = int(input(''))
+                                        if self.cards_union[0][1] == self.cards_union[1][1]:
+                                            cls()
+                                            print('it is a duplicate union, Repeat the play')
+                                        else:
+                                            if player.deck_of_cards[user_actionOne] + self.cards_union[-1] < 14:
+                                                cls()
+                                                self.cards_union.append(player.deck_of_cards.pop(user_actionOne))
+                                                self.cards_union = self.union.add_card(self.cards_union)
+                                                break
+                                            else:
+                                                cls()
+                                                print('the increase does not comply with the rules, Do other action ')
+                                else:
+                                    cls()
+                                    print('There are not cards in union')
+                    
+                            
+
+                        else:
                             print(list(enumerate(player.deck_of_cards)))
-                
-                        
+                            user_action = int(input('Which card would you discard? '))
+                            self.cards_on_table.append((player.deck_of_cards[user_action]))
+                            player.deck_of_cards = list(filter(self.checktable,player.deck_of_cards))
+                            break
+            except:
+                cls()
+                print('ONLY INTEGERS')
 
-                    else:
-                        print(list(enumerate(player.deck_of_cards)))
-                        user_action = int(input('Which card would you discard? '))
-                        self.table.add_card(player.deck_of_cards.pop(user_action))
-                        player.deck_of_cards = list(filter(self.checktable,player.deck_of_cards))
-                        break
-                except:
-                    print('ONLY INTEGERS')
-
-                
-
-                    
-
-
-            
-
-            
-                    
-                    
-                
-            
-            # self.Game.check_winner(self.playerOne,self.playerTwo)
-            
-
-        # make player moves
-        
-
-            # check cards from table to be grabbed
-            # player.deck_of_cards_grabbed = cards to grab
-            # remove matching cards from table
 
     def no_more_cards(self):
         return len(self.deck.cards) == 0 and len(self.player1.deck_of_cards) == 0 and len(self.player2.deck_of_cards) == 0
 
-
-    
-
 play = Play()
 play.run()
-
-
-
-        
